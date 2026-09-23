5534dc5dc1c0e61623c5a82a78ed3173a606f184
kimtae7082-blip <kimtae70822gmail.com>
2026-09-23 15:22:03 +0900

    애니메이션 만들기

--- 애니메이션 만들기/animation.js
+++ 애니메이션 만들기/animation.js
@@ -1,14 +1,25 @@
 const sky = document.querySelector(".sky");
 const button = document.querySelector("#dayNightBtn");
 
+
 button.addEventListener("click", function() {
-    
+
+    // 낮 / 밤 전환
     sky.classList.toggle("day");
 
-    if(sky.classList.contains("day")) {
-        button.textContent = "밤으로 바꾸기";
-    } else {
-        button.textContent = "낮으로 바꾸기";
+
+    // 낮일 때
+    if (sky.classList.contains("day")) {
+
+        button.textContent = "🌙 밤으로 바꾸기";
+
+    }
+
+    // 밤일 때
+    else {
+
+        button.textContent = "☀️ 낮으로 바꾸기";
+
     }
 
 });
